b2657479256e2dece8eaef69888a81640bab43e1
angellui16 <angel092479@gmail.com>
2016-07-25 02:26:58 -0600

    Mantenimiento de monedas

---
+++ Angel y Rodrigo/Código/dll_Moneda/dll_Moneda/Presentación/Moneda.Designer.vb
@@ -0,0 +1,400 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Moneda
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Moneda))
+        Me.txtSimbolo = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.dgvDatos = New System.Windows.Forms.DataGridView()
+        Me.txtMoneda = New System.Windows.Forms.TextBox()
+        Me.txtTipo = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TabControl2 = New System.Windows.Forms.TabControl()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.Label26 = New System.Windows.Forms.Label()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.Label24 = New System.Windows.Forms.Label()
+        Me.Label23 = New System.Windows.Forms.Label()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.Label21 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtSimbolo
+        '
+        Me.txtSimbolo.Location = New System.Drawing.Point(108, 134)
+        Me.txtSimbolo.Name = "txtSimbolo"
+        Me.txtSimbolo.Size = New System.Drawing.Size(143, 20)
+        Me.txtSimbolo.TabIndex = 41
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(60, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 36
+        Me.Label1.Text = "Código:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(56, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 37
+        Me.Label2.Text = "Moneda:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(76, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(190, 25)
+        Me.Label3.TabIndex = 45
+        Me.Label3.Text = "Registro moneda"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 138)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 38
+        Me.Label4.Text = "Símbolo:"
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Enabled = False
+        Me.txtCodigo.Location = New System.Drawing.Point(108, 76)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(55, 20)
+        Me.txtCodigo.TabIndex = 39
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Location = New System.Drawing.Point(18, 202)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(456, 184)
+        Me.TabControl1.TabIndex = 43
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.dgvDatos)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(448, 158)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Monedas"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'dgvDatos
+        '
+        Me.dgvDatos.AllowUserToAddRows = False
+        Me.dgvDatos.AllowUserToDeleteRows = False
+        Me.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDatos.Location = New System.Drawing.Point(0, 4)
+        Me.dgvDatos.Name = "dgvDatos"
+        Me.dgvDatos.ReadOnly = True
+        Me.dgvDatos.Size = New System.Drawing.Size(442, 150)
+        Me.dgvDatos.TabIndex = 28
+        '
+        'txtMoneda
+        '
+        Me.txtMoneda.Location = New System.Drawing.Point(108, 103)
+        Me.txtMoneda.Name = "txtMoneda"
+        Me.txtMoneda.Size = New System.Drawing.Size(143, 20)
+        Me.txtMoneda.TabIndex = 40
+        '
+        'txtTipo
+        '
+        Me.txtTipo.Location = New System.Drawing.Point(108, 164)
+        Me.txtTipo.Name = "txtTipo"
+        Me.txtTipo.Size = New System.Drawing.Size(143, 20)
+        Me.txtTipo.TabIndex = 42
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(24, 168)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 13)
+        Me.Label5.TabIndex = 50
+        Me.Label5.Text = "Tipo de cambio:"
+        '
+        'TabControl2
+        '
+        Me.TabControl2.Location = New System.Drawing.Point(3, 65)
+        Me.TabControl2.Name = "TabControl2"
+        Me.TabControl2.SelectedIndex = 0
+        Me.TabControl2.Size = New System.Drawing.Size(479, 329)
+        Me.TabControl2.TabIndex = 52
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.btnSalir)
+        Me.Panel2.Controls.Add(Me.Label26)
+        Me.Panel2.Controls.Add(Me.btnLimpiar)
+        Me.Panel2.Controls.Add(Me.Label24)
+        Me.Panel2.Controls.Add(Me.Label23)
+        Me.Panel2.Controls.Add(Me.Label22)
+        Me.Panel2.Controls.Add(Me.Label21)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.btnEliminar)
+        Me.Panel2.Controls.Add(Me.btnModificar)
+        Me.Panel2.Controls.Add(Me.btnGuardar)
+        Me.Panel2.Location = New System.Drawing.Point(3, 400)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(479, 83)
+        Me.Panel2.TabIndex = 89
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(428, 64)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(27, 13)
+        Me.Label6.TabIndex = 42
+        Me.Label6.Text = "Salir"
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnSalir.BackgroundImage = Global.dll_Moneda.My.Resources.Resources.inside
+        Me.btnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnSalir.FlatAppearance.BorderSize = 0
+        Me.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSalir.Location = New System.Drawing.Point(411, 3)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(58, 59)
+        Me.btnSalir.TabIndex = 41
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'Label26
+        '
+        Me.Label26.AutoSize = True
+        Me.Label26.Location = New System.Drawing.Point(25, 64)
+        Me.Label26.Name = "Label26"
+        Me.Label26.Size = New System.Drawing.Size(40, 13)
+        Me.Label26.TabIndex = 40
+        Me.Label26.Text = "Limpiar"
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnLimpiar.BackgroundImage = Global.dll_Moneda.My.Resources.Resources.substract
+        Me.btnLimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnLimpiar.FlatAppearance.BorderSize = 0
+        Me.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLimpiar.Location = New System.Drawing.Point(14, 3)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(58, 59)
+        Me.btnLimpiar.TabIndex = 39
+        Me.btnLimpiar.UseVisualStyleBackColor = False
+        '
+        'Label24
+        '
+        Me.Label24.AutoSize = True
+        Me.Label24.Location = New System.Drawing.Point(539, 66)
+        Me.Label24.Name = "Label24"
+        Me.Label24.Size = New System.Drawing.Size(27, 13)
+        Me.Label24.TabIndex = 35
+        Me.Label24.Text = "Salir"
+        '
+        'Label23
+        '
+        Me.Label23.AutoSize = True
+        Me.Label23.Location = New System.Drawing.Point(355, 64)
+        Me.Label23.Name = "Label23"
+        Me.Label23.Size = New System.Drawing.Size(43, 13)
+        Me.Label23.TabIndex = 34
+        Me.Label23.Text = "Eliminar"
+        '
+        'Label22
+        '
+        Me.Label22.AutoSize = True
+        Me.Label22.Location = New System.Drawing.Point(292, 65)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(50, 13)
+        Me.Label22.TabIndex = 33
+        Me.Label22.Text = "Modificar"
+        '
+        'Label21
+        '
+        Me.Label21.AutoSize = True
+        Me.Label21.Location = New System.Drawing.Point(229, 64)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(45, 13)
+        Me.Label21.TabIndex = 32
+        Me.Label21.Text = "Guardar"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(522, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(58, 59)
+        Me.Button1.TabIndex = 31
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnEliminar.BackgroundImage = Global.dll_Moneda.My.Resources.Resources.close2
+        Me.btnEliminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnEliminar.FlatAppearance.BorderSize = 0
+        Me.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEliminar.Location = New System.Drawing.Point(347, 3)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(58, 59)
+        Me.btnEliminar.TabIndex = 30
+        Me.btnEliminar.UseVisualStyleBackColor = False
+        '
+        'btnModificar
+        '
+        Me.btnModificar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnModificar.BackgroundImage = Global.dll_Moneda.My.Resources.Resources.pen
+        Me.btnModificar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnModificar.Enabled = False
+        Me.btnModificar.FlatAppearance.BorderSize = 0
+        Me.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnModificar.Location = New System.Drawing.Point(285, 3)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(58, 59)
+        Me.btnModificar.TabIndex = 29
+        Me.btnModificar.UseVisualStyleBackColor = False
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnGuardar.BackgroundImage = Global.dll_Moneda.My.Resources.Resources.Floppy_disk_512
+        Me.btnGuardar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnGuardar.FlatAppearance.BorderSize = 0
+        Me.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGuardar.Location = New System.Drawing.Point(219, 3)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(58, 59)
+        Me.btnGuardar.TabIndex = 28
+        Me.btnGuardar.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(64, 50)
+        Me.PictureBox1.TabIndex = 44
+        Me.PictureBox1.TabStop = False
+        '
+        'Moneda
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(486, 485)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.txtTipo)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtSimbolo)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.txtMoneda)
+        Me.Controls.Add(Me.TabControl2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Moneda"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Moneda"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSimbolo As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents txtCodigo As System.Windows.Forms.TextBox
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents dgvDatos As System.Windows.Forms.DataGridView
+    Friend WithEvents txtMoneda As System.Windows.Forms.TextBox
+    Friend WithEvents txtTipo As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TabControl2 As System.Windows.Forms.TabControl
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Label26 As System.Windows.Forms.Label
+    Friend WithEvents btnLimpiar As System.Windows.Forms.Button
+    Friend WithEvents Label24 As System.Windows.Forms.Label
+    Friend WithEvents Label23 As System.Windows.Forms.Label
+    Friend WithEvents Label22 As System.Windows.Forms.Label
+    Friend WithEvents Label21 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents btnEliminar As System.Windows.Forms.Button
+    Friend WithEvents btnModificar As System.Windows.Forms.Button
+    Friend WithEvents btnGuardar As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents btnSalir As System.Windows.Forms.Button
+End Class
---
+++ Angel y Rodrigo/Código/dll_Moneda/dll_Moneda/Presentación/Moneda.vb
@@ -0,0 +1,93 @@
+﻿'*****************************************************************************************
+'NOMBRE: DLL_MONEDA
+'FECHA: 04/07/16
+'CREADOR: ANGEL TOBAR
+'DESCRIPCIÓN: MANTENIMIENTO MONEDA
+'DETALLE: AGREGA DIVERSIDAD DE MONEDAS PARA TRABAJAR PRESUPUESTOS
+'MODIFICACIÓN: 20/07/16
+'*****************************************************************************************
+Imports dllConexion
+Imports System.Windows.Forms
+Public Class Moneda
+
+    Private Sub Moneda_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Se conecta y carga datos al grid
+        Conexion.Conectado()
+        MostrarDatos()
+    End Sub
+    Public Sub MostrarDatos()
+
+        'Llena grid con datos de monedas
+        Conexion.Consulta("select * from tbl_frm_moneda2", "tbl_frm_moneda2")
+        dgvDatos.DataSource = Conexion.ds.Tables("tbl_frm_moneda2")
+
+        'Cambia nombre a columnas del grid
+        dgvDatos.Columns(0).HeaderText = "ID"
+        dgvDatos.Columns(1).HeaderText = "Moneda"
+        dgvDatos.Columns(2).HeaderText = "Abreviatura"
+        dgvDatos.Columns(3).HeaderText = "Tipo de cambio"
+
+    End Sub
+
+    Private Sub dgvDatos_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvDatos.CellContentClick
+        'Muestra datos seleccionados de grid en textbox
+        Dim dgv As DataGridViewRow = dgvDatos.Rows(e.RowIndex)
+        txtCodigo.Text = dgv.Cells(0).Value.ToString()
+        txtMoneda.Text = dgv.Cells(1).Value.ToString()
+        txtSimbolo.Text = dgv.Cells(2).Value.ToString()
+        txtTipo.Text = dgv.Cells(3).Value.ToString()
+    End Sub
+
+    Private Sub btnGuardar_Click_1(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        'Guarda datos
+        Dim guardar As String = "insert into tbl_frm_moneda2 values ('" + txtMoneda.Text + "', '" + txtSimbolo.Text + "', '" + txtTipo.Text + "' )"
+        If (Conexion.Insertar(guardar)) Then
+            MessageBox.Show("Datos guardados correctamente")
+            limpiar()
+            MostrarDatos()
+        Else
+            MsgBox("Error al guardar")
+        End If
+    End Sub
+
+    Private Sub btnModificar_Click_1(sender As Object, e As EventArgs) Handles btnModificar.Click
+        Dim modificar As String = "mnd_moneda2= '" + txtMoneda.Text + "', mnd_abreviatura2= '" + txtSimbolo.Text + "', mnd_tipo_cambio= '" + txtTipo.Text + " '"
+        If (Conexion.Modificar("tbl_frm_moneda2", modificar, "id_moneda2=" + txtCodigo.Text)) Then
+            MessageBox.Show("Datos modificados correctamente")
+            limpiar()
+            MostrarDatos()
+        Else
+            MessageBox.Show("Error al modificar")
+        End If
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        If (Conexion.Eliminar("tbl_frm_moneda2", "id_moneda2= " + txtCodigo.Text)) Then
+            MessageBox.Show("Datos eliminados correctamente")
+            limpiar()
+            MostrarDatos()
+        Else
+            MessageBox.Show("Error al eliminar")
+        End If
+    End Sub
+
+
+    Private Sub btnSalir_Click_1(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Dim opc As DialogResult = MsgBox("¿Desea salir de esta aplicación?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Salir")
+
+        If opc = DialogResult.Yes Then
+            Me.Close()
+        Else
+        End If
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        limpiar()
+    End Sub
+    Public Sub limpiar()
+        txtCodigo.Clear()
+        txtMoneda.Clear()
+        txtSimbolo.Clear()
+        txtTipo.Clear()
+    End Sub
+End Class
